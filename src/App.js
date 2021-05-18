19391bb2f0a077d05105719996e41cb57d6bf160
jhkyoon <jhkyoon@naver.com>
2021-05-18 15:40:16 +0900

    Bit_CoinServer_get_BNPrice_And_send_BNPrice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Chart from './Components/Chart';
+import axios from "axios";
 
-function App() {
-  return (
-    <body>
-      <h1>BinanceChart(BTC)</h1>
-      <Chart></Chart>
-    </body>
-  );
+
+
+class App extends Component {
+    state = {
+        isLoading: true,
+        bn_price_data: []
+     };
+    //binance_bitcoin_data 서버에서 불러오기.
+    getPriceData = async () => {
+      const {
+          //ES6에서 JSON 파싱시 아래와 같이 좀더 깔끔한 표현이 가능하다.
+          data: {
+            data: {bn_price_data}
+          }
+      } = await axios.get (
+          "http://localhost:3001/ch_bn_price"
+        );
+        // axios로 부터 get이 완료되면, isLoading상태를 false로 변경하고, movies객체를 세팅한다.
+        this.setState({ bn_price_data, isLoading: false });
+    };
+
+    componentDidMount() {
+      this.getPriceData();
+    }
+
+    render() {
+      //render함수에서 필요한 isLoading, movies 객체를 state로 부터 받아온다.
+      const { isLoading, bn_price_data} = this.state; 
+      return (
+        <a>
+          <h1>BinanceChart(BTC)</h1>
+          {bn_price_data.map(data =>(
+            <Chart
+              id={data.id}
+              time={data.time}
+              open={data.open}
+              high={data.high}
+              low={data.low}
+              close={data.close}
+              volume={data.volume}
+              closeTime={data.closeTime}
+              assetVolume={data.assetVolume}
+              trades={data.trades}
+              buyBaseVolume={data.buyBaseVolume}
+              buyAssetVolume={data.buyAssetVolume}
+              ignored={data.ignored}
+            />
+          ))}
+        </a>
+      );
+    }
+
 }
 
 export default App;
--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,12 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 import { createChart, CrosshairMode } from 'lightweight-charts';
-import { priceData } from './priceData';
+//import { priceData } from './priceData';
 // import { areaData } from './areaData';
 import { volumeData } from './volumeData.js';
+import PropTypes from "prop-types";
+import axios from "axios";
 
 import './Chart.css';
 
-function Chart() {
+//binance data 자료형
+    // time: Number,
+    // open: String,
+    // high: String,
+    // low: String,
+    // close: String,
+    // volume: String,
+    // closeTime: Number,
+    // assetVolume: String,
+    // trades: Number,
+    // buyBaseVolume: String,
+    // buyAssetVolume: String,
+    // ignored: String,
+    // date: { type: Date, default: Date.now }
+
+function Chart({ id, time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored, date }) {
   const chartContainerRef = useRef();
   const chart = useRef();
   const resizeObserver = useRef();
@@ -49,8 +66,19 @@ function Chart() {
       wickUpColor: '#838ca1',
     });
 
-    candleSeries.setData(priceData);
-
+    // async () => {
+    //   axios
+    //     .get("http://localhost:3001/ch_bn_price")
+    //     .then(({ data } ) => {
+    //       console.log(data);
+    //     })
+    //     .catch(e => {
+    //       console.error(e);
+    //     });
+    // };
+   
+    candleSeries.setData({open, high, low, close});
+      
     // const areaSeries = chart.current.addAreaSeries({
     //   topColor: 'rgba(38,198,218, 0.56)',
     //   bottomColor: 'rgba(38,198,218, 0.04)',
@@ -97,5 +125,21 @@ function Chart() {
     </div>
   );
 }
+
+// 위와 같이 Movie라는 component를 생성하면 사용할 수 있습니다. props는 아래 propsTypes를 통해서 정의할 수 있습니다.
+Chart.propTypes = {
+    time: PropTypes.number.isRequired,
+    open: PropTypes.string.isRequired,
+    high: PropTypes.string.isRequired,
+    low: PropTypes.string.isRequired,
+    close: PropTypes.string.isRequired,
+    volume: PropTypes.string.isRequired,
+    closeTime: PropTypes.number.isRequired,
+    assetVolume: PropTypes.string.isRequired,
+    trades: PropTypes.number.isRequired,
+    buyBaseVolume: PropTypes.string.isRequired,
+    buyAssetVolume: PropTypes.string.isRequired,
+    ignored: PropTypes.string.isRequired
+  };
 
 export default Chart;
